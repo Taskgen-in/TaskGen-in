9a0716e8bc84815098040b15e7c46ba13e3af327
task-gen <taskgen.in@gmail.com>
2025-05-26 18:14:59 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -118,7 +118,7 @@ export function Footer() {
         </div>
 
         <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-          <p>&copy; 2024 TaskGen. All rights reserved. | Empowering India's workforce from home.</p>
+          <p>&copy; 2025 TaskGen. All rights reserved. | Empowering India's workforce from home.</p>
         </div>
       </div>
     </footer>
